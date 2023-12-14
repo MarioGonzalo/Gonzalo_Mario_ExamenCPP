16ed25d8d58b3747e73f27b5eb926601318c7b6d
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-12-14 12:52:40 +0100

    Parte 2 e inicio de la 3 CPP

--- EjercicioCPP/main.cpp
+++ EjercicioCPP/main.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 
 using namespace std;
 
-//Inicio parte 1: Estructura del estudiante
-// Se crea una estructura llamada Estudiante donde se almacena el nombre, edad y promedio de un estudiante
-struct Estudiante{
-    char nombre [50];
+// Inicio parte 1: Estructura del estudiante
+struct Estudiante {
+    char nombre[50];
     int edad;
-    int promedio;
+    float promedio;
+    vector<string> materias;
 };
 
-// Se crea una función llamada mostrarEstudiante que muestra los datos del estudiante
-void mostrarEstudiante(Estudiante estudiante){
+struct Asistencia {
+    int fecha;
+    bool asistio;
+    string materia;
+};
+
+void mostrarEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << endl;
     cout << "Edad: " << estudiante.edad << endl;
     cout << "Promedio: " << estudiante.promedio << endl;
+    cout << "Materias: ";
+    for (const string& materia : estudiante.materias) {
+        cout << materia << " ";
+    }
+    cout << endl;
 }
 // Fin parte 1: Estructura del estudiante
 
+// Inicio parte 2: Gestión de materias
+// Se crea un void para agregar materias al vector de materias del estudiante
+void agregarMateria(Estudiante& estudiante, const string& materia) {
+    estudiante.materias.push_back(materia);
+}
+// Fin parte 2: Gestión de materias
+
+// Inicio parte 3: Gestión de asistencias
+// Se crea un void para especificar la asistencia
+void agregarAsistencia(Asistencia& asistencia) {
+    cout << "Ingrese la fecha: ";
+    cin >> asistencia.fecha;
+    cout << "Ingrese la materia: ";
+    cin >> asistencia.materia;
+    cout << "Ingrese la asistencia (1: asistio, 0: no asistio): ";
+    cin >> asistencia.asistio;
+}
+// Fin parte 3: Gestión de asistencias
 int main() {
     Estudiante estudiante;
     strcpy(estudiante.nombre, "Juan");
     estudiante.edad = 20;
     estudiante.promedio = 10;
+
+    agregarMateria(estudiante, "Matematicas");
+
     mostrarEstudiante(estudiante);
+
+    return 0;
 }
