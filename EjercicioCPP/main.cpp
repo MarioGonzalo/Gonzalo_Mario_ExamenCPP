928737b23910650fe91f95e82786f244b35a0475
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-12-14 12:55:57 +0100

    Creación del void "eliminarMaterias" CPP

--- EjercicioCPP/main.cpp
+++ EjercicioCPP/main.cpp
@@ -13,7 +13,9 @@ struct Estudiante {
 };
 
 struct Asistencia {
-    int fecha;
+    int dia;
+    int mes;
+    int ano;
     bool asistio;
     string materia;
 };
@@ -35,13 +37,25 @@ void mostrarEstudiante(const Estudiante& estudiante) {
 void agregarMateria(Estudiante& estudiante, const string& materia) {
     estudiante.materias.push_back(materia);
 }
+void eliminarMateria(Estudiante& estudiante, const string& materia) {
+    for (int i = 0; i < estudiante.materias.size(); ++i) {
+        if (estudiante.materias[i] == materia) {
+            estudiante.materias.erase(estudiante.materias.begin() + i);
+            break;
+        }
+    }
+}
 // Fin parte 2: Gestión de materias
 
 // Inicio parte 3: Gestión de asistencias
 // Se crea un void para especificar la asistencia
 void agregarAsistencia(Asistencia& asistencia) {
-    cout << "Ingrese la fecha: ";
-    cin >> asistencia.fecha;
+    cout << "Ingrese el dia: ";
+    cin >> asistencia.dia;
+    cout << "Ingrese el mes: ";
+    cin >> asistencia.mes;
+    cout << "Ingrese el año: ";
+    cin >> asistencia.ano;
     cout << "Ingrese la materia: ";
     cin >> asistencia.materia;
     cout << "Ingrese la asistencia (1: asistio, 0: no asistio): ";
